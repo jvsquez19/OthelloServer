aecbd3f73e0dc83b5ae6e56f331313691ab5ed8b
Jafeth  Vásquez <jvsquez19@gmail.com>
2018-09-28 20:45:09 -0600

    dep 5

--- server.js
+++ server.js
@@ -7,19 +7,9 @@ var routes = require("./api/routes/WSRoutes")
 var cors = require("cors")
 
 //Variables Globales
-
-
-
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
 
 routes(app)
 socket(http)
