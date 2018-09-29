038e4d4c2676254ae9803608e33b1d7bea22da15
Jafeth  Vásquez <jvsquez19@gmail.com>
2018-09-28 20:36:25 -0600

    dep 4

--- server.js
+++ server.js
@@ -12,10 +12,19 @@ var cors = require("cors")
 
 app.use(express.json())
 app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 routes(app)
 socket(http)
 
-http.listen(3000, () => {
+http.listen(5000, () => {
   console.log('Server started on port 3000');
 });
 
