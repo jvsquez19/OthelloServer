4d1b1071138cebc2a1a882f382eee659173e35d3
Jafeth  Vásquez <jvsquez19@gmail.com>
2018-09-30 17:59:08 -0600

    cambios chivas

--- api/routes/webSocket.js
+++ api/routes/webSocket.js
@@ -1,87 +1,93 @@
 'use strict';
-module.exports = function(http){
-//Dependencias
-const io = require('socket.io')(http);
-//Variables Globales 
-var Conexiones = {}
-var partidasPendientes = []
-var partidasEnCurso = []
+module.exports = function(http) {
+    //Dependencias
+    const io = require('socket.io')(http);
+    //Variables Globales 
+    var Conexiones = {}
+    var partidasPendientes = []
+    var partidasEnCurso = []
 
 
-io.on('connection', (socket) => {
+    io.on('connection', (socket) => {
 
-    console.log('user connected '+ socket.id);
+        console.log('user connected ' + socket.id);
 
-    socket.on('disconnect', function() {
-        console.log('user disconnected');
-    });
+        socket.on('disconnect', function() {
+            console.log('user disconnected');
+        });
 
-    socket.on("get-matches",()=>{
-        io.emit('pendingMatches',{matches:partidasPendientes})
-    })
+        socket.on("get-matches", () => {
+            io.emit('pendingMatches', { matches: partidasPendientes })
+        })
 
-    socket.on('join-match',(data)=>{
-        Conexiones[data.user.uid]=socket.id
-        console.log(data)
-        var joined = partidasPendientes.pop(data.id)
-        joined.player2 = data.user.displayName
-        joined.player2uid = data.user.uid
-        console.log(joined)
-        //Se crea la nueva partida
-        var newGame = require("../../src/gameState")
-        var newGameState = newGame["default"].nuevoJuego(parseInt(joined['size']),1, parseInt(joined['dificultad']), joined);
-        partidasEnCurso.push({gameState:newGameState,pActual:[joined.player1uid,joined.player2uid]})
-        io.sockets.connected[Conexiones[joined.player1uid]].emit("match-created",{id:partidasEnCurso.length-1,state:newGameState.dataAct(),config:newGameState.config})
-        io.sockets.connected[Conexiones[joined.player2uid]].emit("match-created",{id:partidasEnCurso.length-1,state:newGameState.dataAct(),config:newGameState.config})
-    })
+        socket.on('join-match', (data) => {
+            Conexiones[data.user.uid] = socket.id
+            console.log(data)
+            var joined = partidasPendientes.pop(data.id)
+            joined.player2 = data.user.displayName
+            joined.player2uid = data.user.uid
+            console.log(joined)
+                //Se crea la nueva partida
+            var newGame = require("../../src/gameState")
+            var newGameState = newGame["default"].nuevoJuego(parseInt(joined['size']), 1, parseInt(joined['dificultad']), joined);
+            partidasEnCurso.push({ gameState: newGameState, pActual: [joined.player1uid, joined.player2uid] })
+            io.sockets.connected[Conexiones[joined.player1uid]].emit("match-created", { id: partidasEnCurso.length - 1, state: newGameState.dataAct(), config: newGameState.config })
+            io.sockets.connected[Conexiones[joined.player2uid]].emit("match-created", { id: partidasEnCurso.length - 1, state: newGameState.dataAct(), config: newGameState.config })
+        })
 
-    socket.on("create-match",(nuevaPartida)=>{
-        Conexiones[nuevaPartida.player1uid] = socket.id
-        nuevaPartida.socketid = socket.id
-        nuevaPartida.id = partidasPendientes.length
-        console.log(nuevaPartida)
-        partidasPendientes.push(nuevaPartida)
-        io.emit('pendingMatches',{matches:partidasPendientes})
-    })
+        socket.on("start-match", (data) => {
+            console.log("StartServer");
+            var joined = partidasPendientes[data.id];
+            io.sockets.connected[Conexiones[joined.player1uid]].emit("player-found", { player2: data.user.displayName, player2uid: data.user.player2uid })
+        })
 
-    socket.on("played",(data)=>{
-        var current = partidasEnCurso[data.id]
-        if(Conexiones[current.pActual[0]]==socket.id){
-            var ant = current.gameState.turnoJugador
-            current.gameState = current.gameState.jugadaRealizada([data.row,data.column])
-            if(current.gameState.turnoJugador != ant){
-            current.pActual.reverse()
+        socket.on("create-match", (nuevaPartida) => {
+            Conexiones[nuevaPartida.player1uid] = socket.id
+            nuevaPartida.socketid = socket.id
+            nuevaPartida.id = partidasPendientes.length
+            console.log(nuevaPartida)
+            partidasPendientes.push(nuevaPartida)
+            io.emit('pendingMatches', { matches: partidasPendientes })
+        })
+
+        socket.on("played", (data) => {
+            var current = partidasEnCurso[data.id]
+            if (Conexiones[current.pActual[0]] == socket.id) {
+                var ant = current.gameState.turnoJugador
+                current.gameState = current.gameState.jugadaRealizada([data.row, data.column])
+                if (current.gameState.turnoJugador != ant) {
+                    current.pActual.reverse()
+                }
+                partidasEnCurso[data.id] = current
+                io.sockets.connected[Conexiones[current.pActual[0]]]
+                    .emit("moved", current.gameState.dataAct())
+                io.sockets.connected[Conexiones[current.pActual[1]]]
+                    .emit("moved", current.gameState.dataAct())
+            } else {
+                console.log("no es correcto")
             }
-            partidasEnCurso[data.id] = current
-            io.sockets.connected[Conexiones[current.pActual[0]]]
-            .emit("moved",current.gameState.dataAct())
-            io.sockets.connected[Conexiones[current.pActual[1]]]
-            .emit("moved",current.gameState.dataAct())
-        }else{
-            console.log("no es correcto")
-        }
-    })
+        })
 
-    socket.on("match-left",(data)=>{
-        var current = partidasEnCurso[data.id]
-        console.log(current)
-        if(current.gameState.config.player1uid==data.user.uid){
-            io.sockets.connected[Conexiones[current.gameState.config.player2uid]]
-            .emit("match-left")
-        }else{
-            io.sockets.connected[Conexiones[current.gameState.config.player1uid]]
-            .emit("match-left")
-        }
-    })
+        socket.on("match-left", (data) => {
+            var current = partidasEnCurso[data.id]
+            console.log(current)
+            if (current.gameState.config.player1uid == data.user.uid) {
+                io.sockets.connected[Conexiones[current.gameState.config.player2uid]]
+                    .emit("match-left")
+            } else {
+                io.sockets.connected[Conexiones[current.gameState.config.player1uid]]
+                    .emit("match-left")
+            }
+        })
 
-    socket.on("new-connection", (uid)=>{
-        console.log("idSocket: "+socket.id + " uid: "+uid)
-        Conexiones[uid]=socket.id
-        io.sockets.connected[socket.id].emit("message",{type:'new-message',text:"su uid es: "+uid});
-        console.log(Conexiones)
-    })
+        socket.on("new-connection", (uid) => {
+            console.log("idSocket: " + socket.id + " uid: " + uid)
+            Conexiones[uid] = socket.id
+            io.sockets.connected[socket.id].emit("message", { type: 'new-message', text: "su uid es: " + uid });
+            console.log(Conexiones)
+        })
 
-});
+    });
 
 
 }
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 //Dependencias
 const express = require('express')
 const app = express()
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 var socket = require('./api/routes/webSocket')
 var routes = require("./api/routes/WSRoutes")
 var cors = require("cors")
 
 //Variables Globales
-app.use(cors())
+
+
+
 app.use(express.json())
-
-
+app.use(cors())
 routes(app)
 socket(http)
 
-http.listen(process.env.PORT || 3000, () => {
+http.listen(3000, () => {
   console.log('Server started on port 3000');
 });
 
